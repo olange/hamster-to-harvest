99013aa24dd97dfcd29b42c66ad91ff5c13700e8
Olivier Lange <olange@petit-atelier.ch>
2015-08-07 22:17:22 +0200

    Transcription fine des valeurs des attributs d'un élément <activity>

--- src/hamster_to_harvest/core.clj
+++ src/hamster_to_harvest/core.clj
@@ -1,5 +1,6 @@
 (ns hamster-to-harvest.core
   (:require [clojure.java.io :as io]
+            [clojure.string :as str :refer [split]]
             [clojure.xml :as xml]
             [clojure.zip :as zip]
             [clojure.data.zip.xml :as zip-xml])
@@ -15,11 +16,25 @@
       xml/parse
       zip/xml-zip))
 
+(defn activity->map
+  "Given an activity element, return its attributes as a map,
+  converting the source data to Clojure structures"
+  [activity]
+  {
+    :name     (zip-xml/attr activity :name)
+    :category (zip-xml/attr activity :category)
+    :tags     (split (zip-xml/attr activity :tags) #",\s*")
+    :description (zip-xml/attr activity :description)
+    :duration_minutes (Integer/valueOf (zip-xml/attr activity :duration_minutes))
+    :start_time (zip-xml/attr activity :start_time)
+    :end_time (zip-xml/attr activity :end_time)
+  })
+
 (defn activities->xrel
   [root]
   (into #{}
-        (for [activity (zip-xml/xml-> root :activity)]
-          (-> activity first :attrs))))
+        (map activity->map
+             (zip-xml/xml-> root :activity))))
 
 (defn -main
   "Reads a sample Hamster XML export and dumps the parsed content"
