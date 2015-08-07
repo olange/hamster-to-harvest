4ca2edbb83ae01cc4a9486328d96b76f2f656839
Olivier Lange <olange@petit-atelier.ch>
2015-08-07 21:23:14 +0200

    Ajouté projet Leiningen

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to hamster-to-harvest
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ project.clj
@@ -0,0 +1,11 @@
+(defproject hamster-to-harvest "0.1.0-SNAPSHOT"
+  :description "Migrate an XML export of Hamster facts to Harvest entries in CSV format"
+  :url "https://github.com/olange/hamster-to-harvest-csv"
+  :license {:name "Creative Commons Attribution-ShareAlike 4.0 International License"
+            :url  "http://creativecommons.org/licenses/by-sa/4.0/"}
+  :dependencies [
+    [org.clojure/clojure "1.6.0"]
+  ]
+  :main ^:skip-aot hamster-to-harvest.core
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all}})
