f8ac80a4d04472f65dfe7d999ef4e5c79c2cf967
Olivier Lange <olange@petit-atelier.ch>
2015-08-07 21:40:55 +0200

    Lecture document XML

--- src/hamster_to_harvest/core.clj
+++ src/hamster_to_harvest/core.clj
@@ -1,7 +1,20 @@
 (ns hamster-to-harvest.core
+  (:require [clojure.java.io :as io]
+            [clojure.xml :as xml]
+            [clojure.zip :as zip])
   (:gen-class))
 
+(defn read-xml
+  "Given the filename of an XML document, read and parse it,
+  and return an XML zipper over the parsed document"
+  [fname]
+  (-> fname
+      io/resource
+      io/file
+      xml/parse
+      zip/xml-zip))
+
 (defn -main
-  "I don't do a whole lot ... yet."
+  "Reads a sample Hamster XML export and dumps the parsed content"
   [& args]
-  (println "Hello, World!"))
+  (println (read-xml "sample.xml")))
