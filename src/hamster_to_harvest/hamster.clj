653324ac0de04612916f8659dff0250f996595f0
Olivier Lange <olange@petit-atelier.ch>
2015-08-08 12:26:19 +0200

    Introduit defrecord HamsterActivity

--- src/hamster_to_harvest/hamster.clj
+++ src/hamster_to_harvest/hamster.clj
@@ -5,6 +5,15 @@
             [clojure.zip :as zip]
             [clojure.data.zip.xml :as zip-xml]))
 
+(defrecord HamsterActivity [
+  name
+  category
+  tags
+  description
+  duration_minutes
+  start_time
+  end_time])
+
 (defn read-xml
   "Given the filename of an XML document, read and parse it,
   and return an XML zipper over the parsed document"
@@ -16,10 +25,10 @@
       zip/xml-zip))
 
 (defn activity->map
-  "Given an activity element, return its attributes as a map,
-  converting the source data to Clojure structures"
+  "Given an activity element, return a corresponding `HamsterActivity` record,
+  which is a map with all the values of the element, casted to Clojure data structures"
   [activity]
-  {
+  (map->HamsterActivity {
     :name     (zip-xml/attr activity :name)
     :category (zip-xml/attr activity :category)
     :tags     (split (zip-xml/attr activity :tags) #",\s*")
@@ -28,7 +37,7 @@
     :duration_minutes (Integer/valueOf (zip-xml/attr activity :duration_minutes))
     :start_time (zip-xml/attr activity :start_time)
     :end_time (zip-xml/attr activity :end_time)
-  })
+  }))
 
 (defn activities->xrel
   "Given an XML zipper on the root element of an export of Hamster activities,
