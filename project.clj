aa93f16fa1bce1c8f24ac24123916cf34bd07354
Olivier Lange <olange@petit-atelier.ch>
2015-08-09 12:23:25 +0200

    Introduit invocation depuis ligne de commande

--- project.clj
+++ project.clj
@@ -1,11 +1,12 @@
 (defproject hamster-to-harvest "0.1.0-SNAPSHOT"
-  :description "Migrate an XML export of Hamster facts to Harvest entries in CSV format"
+  :description "Migrate from Hamster activities in XML format to Harvest entries in CSV format"
   :url "https://github.com/olange/hamster-to-harvest-csv"
   :license {:name "Creative Commons Attribution-ShareAlike 4.0 International License"
             :url  "http://creativecommons.org/licenses/by-sa/4.0/"}
   :dependencies [
     [org.clojure/clojure "1.6.0"]
     [org.clojure/data.zip "0.1.1"]
+    [org.clojure/tools.cli "0.3.2"]
     [clj-stacktrace "0.2.8"]
   ]
   :main ^:skip-aot hamster-to-harvest.core
--- src/hamster_to_harvest/core.clj
+++ src/hamster_to_harvest/core.clj
@@ -1,16 +1,18 @@
 (ns hamster-to-harvest.core
+  "Core package of the Hamster to Harvest migration utility."
   (:require [hamster-to-harvest.hamster :as hamster]
             [hamster-to-harvest.harvest :as harvest]
             [hamster-to-harvest.mapping :as mapping]
+            [clojure.tools.cli :as cli]
             [clojure.string :as str]
             [clojure.pprint :refer :all])
   (:gen-class))
 
-
-(defn -main
+(defn migrate
   "Reads a sample Hamster XML export and dumps the parsed content"
-  [& args]
-  (let [root (hamster/read-xml "sample.xml")
+  [options arguments]
+  (let [input-filename (first arguments)
+        root (hamster/read-xml input-filename)
         activities (hamster/activities->xrel root)
         time-entries (map mapping/activity->time-entry activities)]
 
@@ -20,3 +22,37 @@
           "\nHarvest Time tracking entries:\n"
           harvest/csv-header-line "\n"
           (str/join "\n" (harvest/as-csv time-entries)))))
+
+(def cli-options [
+  ["-h" "--help" "Show help"]])
+
+(defn usage [banner summary]
+  (str banner
+       "\n\nUsage:"
+       "\n  hamster-to-harvest [options] FILE.xml"
+       "\n\nOptions:\n"
+       summary))
+
+(defn error [banner errors]
+  (str banner
+      "\n\n"
+      (str/join \newline errors)))
+
+(defn exit [status msg]
+   (println msg)
+   (System/exit status))
+
+(defn -main
+  "Reads a sample Hamster XML export and dumps the parsed content"
+  [& args]
+  (let [{:keys [options arguments errors summary]} (cli/parse-opts args cli-options)
+        BANNER "Migrate Hamster activities (XML) to Harvest time tracking entries (CSV)"]
+    (cond
+      (:help options)
+        (exit 0 (usage BANNER summary))
+      (not= 1 (count arguments))
+        (exit 1 (error BANNER ["The input filename of an Hamster XML export is expected."]))
+      errors
+        (exit 1 (error BANNER errors))
+      :else
+        (migrate options arguments))))
