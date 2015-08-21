12a53581042f3e981875f920dc4301067cb43268
Olivier Lange <olange@petit-atelier.ch>
2015-08-21 19:29:27 +0200

    Réalisé mapping projet HTAP

--- src/hamster_to_harvest/mapping.clj
+++ src/hamster_to_harvest/mapping.clj
@@ -15,50 +15,58 @@
   [duration-in-min]
   (/ duration-in-min 60.0)) ;; will cast implicitely to Double
 
-(defn name->client+proj
+(defn name-and-more->client+proj
   "Given the name of an Hamster project, return the corresponding client
   and project name of Harvest; this mapping is specific to each Harvest user"
-  [name]
-  (condp = name
-    "BSAgedco"   ["Régie Brolliet SA" "GED e-mail"]
-    "BSAsupdev"  ["Régie Brolliet SA" "Infrastruct. dév."]
-    "CFDEwebdev" ["Cofideco SA" "Site web cofideco.ch"]
-    [(str "***N" name) (str "***N" name)]))
+  [name category tags]
+  (let [unmatched [(str "***N" name) (str "***N" name)] ]
+    (condp = name
+      "BSAgedco"   ["Régie Brolliet SA" "GED e-mail"]
+      "BSAsupdev"  ["Régie Brolliet SA" "Infrastruct. dév."]
+      "CFDEwebdev" ["Cofideco SA" "Site web cofideco.ch"]
+      "HTAPwebdev" (if (some #{"Publication"} tags)
+                     ["Fondation Horst Tappe" "Maintenance du site"]
+                     ["Fondation Horst Tappe" "Création du site"]))))
 
 (defn category+tags-and-more->task
   "Given the category and tags (a vector of strings) of an Hamster activity,
   return the matching task for Harvest; this mapping is specific to each Harvest user"
   [category tags name]
-  (cond
-    ;; Mappings common to all projects
-    (some #{"Développement"} tags) "Développement"                  ;; BSA  CFDE
-    (some #{"Documentation"} tags) "Documentation"                  ;; BSA  CFDE
-    (some #{"Support"} tags) "Support"                              ;; BSA
-    (some #{"Système"} tags) "Admin. système"                       ;; BSA  CFDE
-    (some #{"Tests"} tags) "Tests intégration"                      ;; BSA
-    (some #{"Déplacement"} tags) "Déplacement"                      ;; CFDE
-    (some #{"Etude et veille"} tags) "Recherche et veille tech."    ;; CFDE
-    (some #{"Publication"} tags) "Actualisation du site"            ;; CFDE
-    (some #{"Séance de travail"} tags) "Séance de travail"          ;; CFDE
+  (let [unmatched (str "***C" category ";T" (join \, tags))]
+    (cond
+      ;; Mappings common to all projects
+      (some #{"Déplacement"} tags)            "Déplacement"               ;; CFDE, HTAP
+      (some #{"Développement"} tags)          "Développement"             ;; BSA, CFDE, HTAP
+      (some #{"Documentation"} tags)          "Documentation"             ;; BSA, CFDE
+      (some #{"Support"} tags)                "Support"                   ;; BSA, HTAP
+      (some #{"Système"} tags)                "Admin. système"            ;; BSA, CFDE, HTAP
+      (some #{"Tests"} tags)                  "Tests intégration"         ;; BSA
+      (some #{"Etude et veille"} tags)        "Recherche et veille tech." ;; CFDE
+      (some #{"Publication"} tags)            "Actualisation du site"     ;; CFDE, HTAP
+      (some #{"Séance de travail"} tags)      "Séance de travail"         ;; CFDE, HTAP
 
-    ;; Mappings specific to given projects
-    (= name "CFDEwebdev")
-      (cond
-        (some #{"Conception"} tags) "Design fonctionnel"
-        (some #{"Coordination"} tags)
-          (if (= "nonFacturé" category) "Administration" "Suivi de projet")
-        (some #{"Enseignement"} tags) "Enseignement / Formation"    ;; CFDE
-        (some #{"Design fonctionnel"} tags) "Design fonctionnel"    ;; CFDE
-        (some #{"Design graphique"} tags) "Design graphique"        ;; CFDE
-        (some #{"Stratégie et prospection"} tags) "Administration"  ;; CFDE
-        :else (str "***C" category ";T" (join \, tags)))
-    (= name "BSA*")
-      (cond
-        (some #{"Conception"} tags) "Conception"
-        (some #{"Coordination"} tags) "Suivi de projet"
-        :else (str "***C" category ";T" (join \, tags)))
-    ;; Unknown categories and tasks
-    :else (str "***C" category ";T" (join \, tags))))
+      ;; Mappings specific to given projects
+      (= name "HTAPwebdev")                                               ;; HTAP
+        (if (some #{"Coordination"} tags)     "Suivi de projet"
+                                              unmatched)
+      (= name "CFDEwebdev")                                               ;; CFDE
+        (cond
+          (some #{"Conception"} tags)         "Design fonctionnel"
+          (some #{"Coordination"} tags)
+            (if (= "nonFacturé" category)     "Administration"
+                                              "Suivi de projet")
+          (some #{"Enseignement"} tags)       "Enseignement / Formation"
+          (some #{"Design fonctionnel"} tags) "Design fonctionnel"
+          (some #{"Design graphique"} tags)   "Design graphique"
+          (some #{"Stratégie et prospection"} tags) "Administration"
+          :else unmatched)
+      (= name "BSA*")                                                     ;; BSA
+        (cond
+          (some #{"Conception"} tags)         "Conception"
+          (some #{"Coordination"} tags)       "Suivi de projet"
+          :else unmatched)
+      ;; Unknown categories and tasks
+      :else unmatched)))
 
 (defn description-and-more->notes
   "Given the description of an Hamster activity, as well as its category
@@ -83,7 +91,7 @@
   ;; how the activity record is factored
   (let [{:keys [name category tags description duration_minutes start_time]} activity
         date              (starttime->date start_time)
-        [client project]  (name->client+proj name)
+        [client project]  (name-and-more->client+proj name category tags)
         task              (category+tags-and-more->task category tags name)
         notes             (description-and-more->notes description category tags)
         hours             (duration->hours duration_minutes)]
